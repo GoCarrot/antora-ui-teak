848ac5e8f35d433156179959afa79e646f015df1
Alex Scarborough <alex@teak.io>
2022-01-28 14:32:34 -0800

    Allow DocsDeploy to get invalidation status.

--- terraform/main.tf
+++ terraform/main.tf
@@ -217,7 +217,8 @@ data "aws_iam_policy_document" "docs-deploy" {
 
   statement {
     actions = [
-      "cloudfront:CreateInvalidation"
+      "cloudfront:CreateInvalidation",
+      "cloudfront:GetInvalidation"
     ]
 
     resources = [
