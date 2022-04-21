000845d221360d63d2cb3a79a0d2a1757e509246
Alex Scarborough <alex@teak.io>
2022-04-21 15:36:51 -0700

    TF: Fetch new ACM certificate with naming standardized in production.

--- terraform/main.tf
+++ terraform/main.tf
@@ -77,7 +77,7 @@ data "aws_route53_zone" "zone" {
 }
 
 data "aws_acm_certificate" "certificate" {
-  domain      = var.domain_root
+  domain      = "*.${var.domain_root}"
   statuses    = ["ISSUED"]
   most_recent = true
 }
